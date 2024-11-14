836954c512bff7623cf2df74358b734a6636d76f
Conor Dunne <conordunne@outlook.ie>
2024-11-14 16:41:20 +0900

    Updates

--- app/roulette/page.jsx
+++ app/roulette/page.jsx
@@ -1,6 +1,8 @@
 "use client"
 import React, { useState, useRef, useEffect } from 'react'
 import dynamic from 'next/dynamic';
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+
 
 const Wheel = dynamic(() => import('react-custom-roulette').then((mod) => mod.Wheel), { ssr: false });
 
@@ -9,6 +11,7 @@ export default function Roulette() {
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [inputValue, setInputValue] = useState(4)
   const [displayedValue, setDisplayedValue] = useState(4);
+  const [confetti, setConfetti] = useState(false)
   
   const drumRoll = useRef(null);
   const cymbal = useRef(null);
@@ -21,6 +24,13 @@ export default function Roulette() {
       cymbal.current = new Audio('/sounds/cymbal.mp3');
     }
   }, []);
+
+  useEffect(() => {
+    if (confetti) {
+      const timer = setTimeout(() => setConfetti(false), 3000); // 4 seconds
+      return () => clearTimeout(timer); // Clean up timer on component unmount or when confetti state changes
+    }
+  }, [confetti]);
 
   const playDrumRoll = () => {
     drumRoll.current?.play();
@@ -44,7 +54,7 @@ export default function Roulette() {
   }
 
   function generateOptions(max) {
-    return Array.from({ length: max }, (_, i) => ({ option: String(i) }));
+    return Array.from({ length: max }, (_, i) => ({ option: String(i + 1) }));
   }
 
   const handleInputChange = (e) => {
@@ -60,6 +70,8 @@ export default function Roulette() {
     setDisplayedValue(inputValue);
     setInputValue('');
   };
+
+ 
   
   const data = generateOptions(displayedValue);
 
@@ -78,9 +90,10 @@ export default function Roulette() {
         </form>
         <p>Max 100</p>
       </div>
+      <div className=' relative'>
       <Wheel
         mustStartSpinning={mustSpin}
-        prizeNumber={prizeNumber}
+        prizeNumber={prizeNumber - 1}
         data={data}
         spinDuration={1}
         outerBorderWidth={0}
@@ -93,9 +106,31 @@ export default function Roulette() {
           setMustSpin(false);
           stopDrumRoll();
           playCymbal();
+          document.getElementById('my_modal_3').showModal()
+          setConfetti(true)
         }}
       />
-      <button onClick={handleSpinClick} className=' bg-green-700 text-white p-4 rounded-md'>SPIN</button>
+            <button onClick={handleSpinClick} className=' bg-green-700 border border-white shadow-2xl text-white rounded-full absolute top-[50%] left-[50%] z-10 transform -translate-x-1/2 -translate-y-1/2 w-1/5 h-1/5'>SPIN</button>
+      </div>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+{/* <button className="btn" onClick={()=>document.getElementById('my_modal_3').showModal()}>open modal</button> */}
+<dialog id="my_modal_3" className="modal">
+  <div className="modal-box flex flex-col items-center justify-between h-3/5">
+    <form method="dialog">
+      {/* if there is a button in form, it will close the modal */}
+      <button 
+      onClick={()=> setConfetti(false)}
+      className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    <h1 className="font-bold text-7xl">{prizeNumber}</h1>
+    {/* <p className="py-4">Press ESC key or click on ✕ button to close</p> */}
+    {/* <div className=' flex w-full justify-around gap-4'>
+      <button className=' flex-grow border-4 border-slate-800 p-4 rounded-md'>Remove Choice?</button>
+      <button className='flex-grow border-4 border-slate-800 p-4 rounded-md' >Close</button>
+    </div> */}
+  </div>
+  {confetti && <Fireworks autorun={{ speed: 3, }} />}
+</dialog>
     </div>
   )
 }
